e6c95822989ce8b1e9922fdbee06157121f84b59
Felipe <flipeoliveira007@gmail.com>
2022-07-13 11:25:22 -0300

    v1

--- aula/v1/04-tipos-de-dados.php
+++ aula/v1/04-tipos-de-dados.php
@@ -19,8 +19,59 @@ if(is_string($idade)):
     echo "É um inteiro";
 else:
     echo "Não é um inteiro";
-endif   
-
+endif;   
 echo "<hr>";
+
+// Float
+$altura = 1.7;
+var_dump($altura);
+if(is_int($altura)){
+    echo "É um inteiro";
+}else{
+    echo "Não é um inteiro";
+}
+echo "<hr>";
+// Boolean
+$admin = false;
+var_dump($admin);
+if(is_int($altura)){
+    echo "É um boolean";
+}else{
+    echo "Não é boolean";
+}
+echo "<hr>";
+//----------Compostos-----------
+
+$carros = array("Gol", "Uno", "Camaro", 12, 12.4, false);
+var_dump($carros);
+if(is_array($carros)){
+ echo "É um array";
+}else{
+    echo "Não é um array";
+}
+echo "<hr>";
+// Object
+class Cliente{
+    public $nome;
+    public function atribuitNome($nome){
+        $this.$nome = $nome;
+    }
+}
+$cliente = new Cliente();
+$cliente->atribuitNome("Felipe");
+var_dump($cliente);
+if(is_object($carros)){
+    echo "É um object";
+   }else{
+       echo "Não é um object";
+   }
+   echo "<hr>";
+
+//----------Especiais-----------  
+//NULL
+$cidade = NULL;
+var_dump($cidade);
+// Resource
+
 
 ?>
---
+++ aula/v1/05-aspas-concatenacao.php
@@ -0,0 +1,4 @@
+<?php
+$nome = 'Felipe Oliveira';
+echo 'Meu nome é'.$nome.'e minha idade é \'10\'';
+?>
